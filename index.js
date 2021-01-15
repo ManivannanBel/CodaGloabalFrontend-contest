2bf775278be278c6868afa743dda9478f80411d3
= <=>
2021-01-15 17:18:59 +0530

    5000 rt error change

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "index.html"));
 });
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}`);
